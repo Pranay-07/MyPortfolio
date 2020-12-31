035300190aef61160d36be2894fbb4f182ce6aee
Kumar,Pranay <Pranay.Kumar@philips.com>
2020-12-31 10:41:24 +0530

    Animation added

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about-me.component.scss']
 })
 export class AboutMeComponent implements OnInit {
-  ImageArr=["../../assets/ReactLogo.jpg","../../assets/AngularLogo.png","../../assets/CLogo.png","../../assets/DockerLogo.png"]
+  ImageArr=["../../assets/ReactLogo.jpg","../../assets/AngularLogo.png","../../assets/CLogo.png","../../assets/DockerLogo.png","../../assets/JasmineLogo.png","../../assets/JavascriptLogo.png","../../assets/HtmlLogo.png","../../assets/CssLogo.png"]
   imagesource1=""
   imagesource2=""
   imagesource3=""
+  imagesource4=""
  public show=false;
   constructor() {
     this.show=false;
@@ -17,10 +18,12 @@ export class AboutMeComponent implements OnInit {
     var i=0;
     var j=1;
     var k=2;
+    var l=3;
       setInterval(()=>{
           this.imagesource1=this.ImageArr[i++%this.ImageArr.length];
           this.imagesource2=this.ImageArr[j++%this.ImageArr.length];
           this.imagesource3=this.ImageArr[k++%this.ImageArr.length];
+          this.imagesource4=this.ImageArr[l++%this.ImageArr.length];
       },1000)
      
       
